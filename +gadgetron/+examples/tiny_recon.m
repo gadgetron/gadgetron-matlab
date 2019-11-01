0b534f4bbcbcd9cc90e060a2c1768715f3e3f81e
Kristoffer Langeland Knudsen <kristofferlknudsen@gradientsoftware.net>
2019-11-01 15:21:05 +0100

    Adding small single-file example, illustrating a more iterative approach.

---
+++ +gadgetron/+examples/tiny_recon.m
@@ -0,0 +1,37 @@
+function tiny_recon(connection)
+    
+    tic;
+
+    try
+        % Once the connection is consumed, calling next will throw an
+        % exception. We wrap the loop in a try here to avoid printing an
+        % error. 
+        
+        while true
+            recon_data = connection.next();
+
+            data = permute( ... 
+                recon_data.bits.buffer.data, ...
+                [4, 1, 2, 3] ...
+            );
+
+            data = gadgetron.lib.fft.cifftn(data, [2, 3, 4]);
+            data = sqrt(sum(square(abs(data)), 1));
+
+            image = ismrmrd.Image.from_data(data, reference_header(recon_data));
+            image.header.image_type = ismrmrd.Image.MAGNITUDE;
+
+            disp("Sending image to client.");
+            connection.send(image);
+        end
+    end
+    
+    toc;
+end
+
+function reference = reference_header(recon_data)
+    % We pick the first header from the header arrays - we need it to initialize the image meta data.    
+    reference = structfun(@(arr) arr(1, :), recon_data.bits.buffer.headers, 'UniformOutput', false);
+end
+
+function x = square(x), x = x .^ 2; end
