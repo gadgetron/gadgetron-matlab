e0c765b46d97c8ab1df532f99e15958f3d09e2ff
Kristoffer Langeland Knudsen <kristofferlknudsen@gradientsoftware.net>
2019-12-17 13:39:09 +0100

    Fixed issue where calling 'gadgetron.external.listen' would leave Matlab unresponsive (ignoring Ctrl+C).

--- +socket/listen.m
+++ +socket/listen.m
@@ -1,5 +1,18 @@
 function sock = listen(port)
     server = java.net.ServerSocket(port);
-    sock = socket.Socket(server.accept());
+    server.setReuseAddress(true);
+    server.setSoTimeout(1000); 
+
+    % Accept would block forever in previous versions, leaving Matlab
+    % unresponsive (process was blocked, so Ctrl+C was ignored). 
+    % We set a timeout, ignoring the exception, to give the user a 
+    % chance to interrupt the listen call. 
+    
+    while true
+        try
+            sock = socket.Socket(server.accept());
+            break        
+        end
+    end
 end
 
