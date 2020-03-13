75246e74c9566686bc360cc2a3b3d4f63bf7bb4c
Kristoffer Langeland Knudsen <princess@localhost.localdomain>
2020-03-13 14:52:47 +0100

    Adding some documentation to Connection; fixed issue where while-true-try-end loop would hide important exceptions.

--- +gadgetron/+external/listen.m
+++ +gadgetron/+external/listen.m
@@ -1,9 +1,21 @@
 function listen(port, handler, varargin)
-    % LISTEN Wait for a connection from Gadgetron (or other ISMRMRD client).
+    % LISTEN Wait for a connection from Gadgetron (or other client).
     % 
     %   LISTEN(PORT, HANDLER, ...) 
     % 
-    % For more information, see <a href="">the Gadgetron Wiki</a>. 
+    % LISTEN will listen for incoming connections on the specified port.
+    % When a connection is ready, HANDLER is called with a Connection 
+    % object as the first argument. 
+    % 
+    % Any additional arguments provided to listen will be forwarded to
+    % HANDLER, when a connection is ready.
+    % 
+    % Example: 
+    %   LISTEN(18000, @(~, a, b) disp([a, ', ', b]), 'Hello', 'World')
+    % 
+    % Will print 'Hello, World' when a connection is established.  
+    %
+    % For more information, see <a href="https://github.com/gadgetron/gadgetron/wiki">the Gadgetron Wiki</a>. 
 
     fprintf("Starting external MATLAB module '%s' in state: [PASSIVE]\n", func2str(handler))
     fprintf("Waiting for connection from client on port: %d\n", port)
