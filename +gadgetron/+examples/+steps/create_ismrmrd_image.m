9118002d203fbeafe81d80c1f57c3d9ceec1530d
Stanislas <stanislas.rapacchi@univ-amu.fr>
2020-05-27 14:40:35 +0200

    Adapted steps to accept N and S dimensions in buffer. Code remains compatible with other calls.

--- +gadgetron/+examples/+steps/create_ismrmrd_image.m
+++ +gadgetron/+examples/+steps/create_ismrmrd_image.m
@@ -1,8 +1,15 @@
 function next = create_ismrmrd_image(input)
-
-    function image = create_image(image)
-        image = gadgetron.types.Image.from_data(image.data, image.reference);
-        image.header.image_type = gadgetron.types.Image.MAGNITUDE;
+    n=1;
+    s=1;
+    function imageout = create_image(image)
+        %init
+        imageout(1,1)=gadgetron.types.Image.from_data(zeros(128),ismrmrd.ImageHeader());
+        for n=1:size(image.reference,1)         
+            for s=1:size(image.reference,2)
+        imageout(n,s) = gadgetron.types.Image.from_data(image.data(:,:,:,:,n,s), image.reference(n,s));
+        imageout(n,s).header.image_type = gadgetron.types.Image.MAGNITUDE;
+            end
+        end
     end
 
     next = @() create_image(input());
--- +gadgetron/+examples/+steps/send_image_to_client.m
+++ +gadgetron/+examples/+steps/send_image_to_client.m
@@ -1,8 +1,13 @@
 function next = send_image_to_client(input, connection)
-
+    n=1;
+    s=1;
     function send_image(image)
-        disp("Sending image to client.");
-        connection.send(image);
+        for n=1:size(image,1)
+            for s=1:size(image,2)
+  fprintf("Sending image %d/%d %d/%d to client.\n",n,size(image,1),s,size(image,2));
+  connection.send(image(n,s));
+            end
+        end
     end
 
     next = @() send_image(input());
